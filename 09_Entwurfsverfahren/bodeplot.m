12be2b322eb55625d0d4d15332d11b2373437c7d
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-07-06 16:59:05 +0200

    Don't plot D or I gains if gains are at limit.

--- 09_Entwurfsverfahren/bodeplot.m
+++ 09_Entwurfsverfahren/bodeplot.m
@@ -2,9 +2,22 @@ function bodeplot(k,Ti,Td)
 
 s = tf('s');
 
+if Td > 1e-3
+    Gd = Td*s;
+else
+    Gd = 0;
+end
+if Ti < 1e2
+    Gi = 1/(Ti*s);
+else
+    Gi = 0;
+end
+
+G0 = (1 + Gi + Gd)*tf([0 0 8.2631 1.6702],[1 0.6915 0.1206 0.0779]);
+
 % Uebertragungsfunktionen
 figure(1)
-bode(k*(1 + 1/(Ti*s) + Td*s)*tf([0 0 8.2631 1.6702],[1 0.6915 0.1206 0.0779]), {1e-3 1e3})
+bode(k*G0, {1e-3 1e3})
 
 grid on
 
